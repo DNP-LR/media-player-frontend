4d2d801eaa7b56ed3e5fe5c75ebc2f0aac9e1f32
Noubissie Pavel <noubissiepavel@gmail.com>
2024-09-10 14:37:21 +0100

    [feat] - pages

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,7 +1,12 @@
 import {createBrowserRouter} from "react-router-dom";
 import AppLayout from "../ui/layout/AppLayout";
-import HomeView from "../pages/HomeView";
-import {AppRoutes} from "../pages/AppRoutes";
+import {AppRoutes} from "./AppRoutes";
+import HomeView from "../pages/home/HomeView";
+import PlayList from "../pages/playlist/PlayList";
+import React from "react";
+import PlayView from "../pages/play/PlayView";
+import PlayWithLyricsView from "../pages/playWithLyrics/PlayWithLyrics";
+
 
 export const router = createBrowserRouter([
     {
@@ -11,6 +16,18 @@ export const router = createBrowserRouter([
                 index: true,
                 path: AppRoutes.HOME,
                 element: <HomeView/>
+            },
+            {
+                path: AppRoutes.PLAYLIST,
+                element: <PlayList/>
+            },
+            {
+                path: AppRoutes.PLAY,
+                element: <PlayView/>
+            },
+            {
+                path: AppRoutes.LYRICS,
+                element: <PlayWithLyricsView/>
             },
             {
                 path: '*',
