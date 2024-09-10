038dd400cd51e18269f809f1653d0ab206de2ba7
Noubissie Pavel <noubissiepavel@gmail.com>
2024-09-10 14:38:03 +0100

    [feat] - pages

---
+++ src/pages/usePage.ts
@@ -0,0 +1,28 @@
+import {useNavigate} from "react-router-dom";
+import {AppRoutes} from "../routes/AppRoutes";
+
+export interface PageBehavior {
+    handleNavigateToPlay: () => void;
+    handleNavigateToPlayWithLyrics: () => void;
+    handleNavigateToPlayList: () => void;
+}
+
+export const usePage = (): PageBehavior => {
+    const navigate = useNavigate();
+
+    const handleNavigateToPlay = () => {
+        navigate(AppRoutes.PLAY)
+    }
+    const handleNavigateToPlayWithLyrics = () => {
+        navigate(AppRoutes.LYRICS)
+    }
+    const handleNavigateToPlayList = () => {
+        console.log('testing')
+        navigate(AppRoutes.PLAYLIST);
+    }
+    return {
+        handleNavigateToPlay,
+        handleNavigateToPlayList,
+        handleNavigateToPlayWithLyrics,
+    }
+}
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,5 +3,5 @@ declare module '.env' {
     const env: {
         MEDIA_PLAYER_ENDPOINT: 'http://localhost:8080'
     };
-    export default env
 }
+export default env
