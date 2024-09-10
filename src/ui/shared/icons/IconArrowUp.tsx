a00d5b2235a19eea3a92555a34c87d7d591ccfda
Noubissie Pavel <noubissiepavel@gmail.com>
2024-09-10 14:37:35 +0100

    [feat] - pages

---
+++ src/ui/shared/icons/IconArrowUp.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function IconArrowUp(props: React.SVGProps<SVGSVGElement>) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+             viewBox="0 0 24 24"
+             stroke-width="1.5"
+             stroke="currentColor"
+             {...props}
+             className="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5"/>
+        </svg>
+
+    )
+}
+
+export default IconArrowUp
