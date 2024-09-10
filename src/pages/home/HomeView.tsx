8fb436a9d96a6d71df436fd6a5bebb2c5a79a79b
Noubissie Pavel <noubissiepavel@gmail.com>
2024-09-10 14:56:39 +0100

    [feat] - images

--- src/pages/home/HomeView.tsx
+++ src/pages/home/HomeView.tsx
@@ -9,7 +9,7 @@ const HomeView = () => {
     }
     return (
         <section className="bg-primaryColor-primaryOne h-screen w-screen flex items-center justify-center">
-            <img className="h-full w-full object-cover" src="/public/Thumbnail.png" alt="Thumbnail"/>
+            <img className="h-full w-full object-cover" src="/src/assets/Thumbnail.png" alt="Thumbnail"/>
             <button
                 onClick={handleNavigateToPlaylist}
                 className="absolute bottom-0  w-full max-w-[40%] font-sans hover:bg-secondaryColor-secondaryTwo :transition duration-500 ease-in hover:ease-out  hover:transition hover:duration-500 text-primaryColor-primaryOne rounded p-4 m-4 bg-secondaryColor-secondaryOne"
