0c9d1953b1812ed9badd2e121f00167a493f493c
Noubissie Pavel <noubissiepavel@gmail.com>
2024-09-10 14:38:17 +0100

    [feat] - pages

---
+++ src/pages/playWithLyrics/PlayWithLyrics.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import PlayWithLyricsViewMobile from "./PlayWithLyricsViewMobile";
+import {usePage} from "../usePage";
+
+const PlayWithLyricsView = () => {
+    const {handleNavigateToPlay} = usePage();
+
+    return (
+        <>
+            <section
+                className="bg-primaryColor-primaryOne h-screen w-screen flex items-center justify-center">
+                <PlayWithLyricsViewMobile
+                    handleNavigateToPlay={handleNavigateToPlay}
+                />
+            </section>
+        </>
+    )
+}
+
+export default PlayWithLyricsView;
