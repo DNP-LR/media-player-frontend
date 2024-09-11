f30576387c5b3c4e60248d30689b3dff29a39c6b
Noubissie Pavel <noubissiepavel@gmail.com>
2024-09-11 10:31:34 +0100

    [refactor] - navigating structure

---
+++ src/ui/layout/WrapperPage.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {twMerge} from "tailwind-merge";
+
+const WrapperPage: React.FC<{ className?: string, children: React.ReactNode }> = ({children, className}) => {
+    return (
+        <div className={
+            twMerge(
+                "bg-primaryColor-primaryOne h-screen w-screen flex items-center justify-center",
+                className
+            )
+        }>
+            {children}
+        </div>
+    )
+}
+
+export default WrapperPage;
