4c7da4a0d470ece6384c64c7d6fb826061e2e4f3
Noubissie Pavel <noubissiepavel@gmail.com>
2024-09-11 19:32:02 +0100

    [fix] - right adapter corrected

--- src/lib/mediaPlayer/infrastructure/gateway/MediaPlayerApiGatewayHttp.ts
+++ src/lib/mediaPlayer/infrastructure/gateway/MediaPlayerApiGatewayHttp.ts
@@ -1,25 +1,24 @@
 import {MediaPlayerApiGateway} from "../../gateway/MediaPlayerApiGateway";
-import {GetMediaPlayerResponse} from "../../usecases/GetMediaPlayer/GetMediaPlayerResponse";
+import {GetAllMediaPlayerResponse} from "../../usecases/GetMediaPlayer/GetAllMediaPlayerResponse";
 import {HttpProvider} from "../../../shared/gateway/HttpProvider";
 import {MediaPlayerRoutes} from "../../../../routes/ApiRoutes";
 import {ErrorState} from "../../../shared/domain/exceptions/ErrorState";
 import {GetMediaPlayerResponseFactoryFromGateway} from "../factories/GetMediaPlayerResponseFactoryFromGateway";
 
 export default class MediaPlayerApiGatewayHttp extends HttpProvider implements MediaPlayerApiGateway {
-    async getMediaPlayerList(): Promise<GetMediaPlayerResponse> {
+
+    async getMediaPlayerList(): Promise<GetAllMediaPlayerResponse[]> {
         let response: Response;
         try {
             response = await this.get(MediaPlayerRoutes.ALL);
-
-            // eslint-disable-next-line @typescript-eslint/no-unused-vars
         } catch (e) {
+            console.log(e)
             throw ErrorState.InternetError();
         }
-        if (response.status === 2000) {
+        if (response.status === 200) {
             const result = await response.json();
             return GetMediaPlayerResponseFactoryFromGateway(result);
         }
         throw ErrorState.ServerError()
     }
-
 }
