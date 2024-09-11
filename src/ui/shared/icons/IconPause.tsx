6e5827508e930ae9c0a332258ba53d17c0adb027
Noubissie Pavel <noubissiepavel@gmail.com>
2024-09-11 19:31:05 +0100

    [fix] - right adapter corrected

--- src/ui/shared/icons/IconPause.tsx
+++ src/ui/shared/icons/IconPause.tsx
@@ -2,9 +2,15 @@ import React from "react";
 
 function IconPause(props: React.SVGProps<SVGSVGElement>) {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+        <svg xmlns="http://www.w3.org/2000/svg"
+             fill="none" viewBox="0 0 24 24"
+             strokeWidth="1.5"
+             {...props}
+             stroke="currentColor"
              className="size-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25v13.5m-7.5-13.5v13.5"/>
+            <path strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.75 5.25v13.5m-7.5-13.5v13.5"/>
         </svg>
     )
 }
