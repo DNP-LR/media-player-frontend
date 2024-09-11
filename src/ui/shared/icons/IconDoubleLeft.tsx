4d6b4d0ce2e07525967a94d2506958dc979e6032
Noubissie Pavel <noubissiepavel@gmail.com>
2024-09-11 19:31:19 +0100

    [fix] - right adapter corrected

--- src/ui/shared/icons/IconDoubleLeft.tsx
+++ src/ui/shared/icons/IconDoubleLeft.tsx
@@ -2,11 +2,17 @@ import React from "react";
 
 function IconDoubleLeft(props: React.SVGProps<SVGSVGElement>) {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+        <svg xmlns="http://www.w3.org/2000/svg"
+             fill="none" viewBox="0 0 24 24"
+             strokeWidth="1.5"
+             stroke="currentColor"
              className="size-6"
              {...props}
         >
-            <path stroke-linecap="round" stroke-linejoin="round" d="m18.75 4.5-7.5 7.5 7.5 7.5m-6-15L5.25 12l7.5 7.5"/>
+            <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m18.75 4.5-7.5 7.5 7.5 7.5m-6-15L5.25 12l7.5 7.5"/>
         </svg>
     )
 }
