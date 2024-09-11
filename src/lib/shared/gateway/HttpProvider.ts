33e1480d7467ad61285bc038cfd36674dd678310
Noubissie Pavel <noubissiepavel@gmail.com>
2024-09-11 19:30:51 +0100

    [fix] - right adapter corrected

--- src/lib/shared/gateway/HttpProvider.ts
+++ src/lib/shared/gateway/HttpProvider.ts
@@ -1,15 +1,18 @@
 export abstract class HttpProvider {
-    private baseUrl: string = import.meta.env.MEDIA_PLAYER_ENDPOINT;
+    // private baseUrl: string = env.MEDIA_PLAYER_ENDPOINT;
+    private baseUrl: string = 'http://localhost:8080/';
 
-    public async get(url: string): Promise<Response> {
-
-        return await fetch(this.getUrl(url), {
+    public async get(url: string, query?: object): Promise<Response> {
+        let queryUrl: string = '';
+        if (query) {
+            queryUrl = '?' + new URLSearchParams(Object.entries(query)).toString();
+        }
+       return  await fetch(this.getUrl(url + queryUrl), {
             headers: {
                 'Content-Type': 'application/json'
             }
         });
     }
-
 
     private getUrl(url: string) {
         return this.baseUrl + url
