368e46e99dbdf030b9cf92d07c1efbe7a9b47be3
Noubissie Pavel <noubissiepavel@gmail.com>
2024-09-11 10:30:51 +0100

    [refactor] - navigating structure

---
+++ src/pages/play/PlayMusic.tsx
@@ -0,0 +1,29 @@
+import {usePage} from "../usePage";
+
+const PlayMusic = () => {
+    const {handleNavigateToPlayWithLyrics} = usePage()
+    return (
+        <section className="">
+            <div className="flex items-center justify-center  shadow-amber-500 ">
+                <img className="h-full max-h-[60%] animate-spin  max-w-[60%] w-full rounded-full"
+                     src="Union.png"
+                     alt="Union"/>
+            </div>
+            <div
+                className="flex flex-col cursor-pointer items-center justify-center h-full max-h-[80%] text-sm text-center"
+                onClick={handleNavigateToPlayWithLyrics}>
+                <span className="">The Monster The Monster</span>
+                <span
+                    className="font-primary text-secondaryColor-secondaryOne ">The Monster The Monster The Monster</span>
+                <span>The Monster The Monster The Monster</span>
+                <span>The Monster The Monster The Monster</span>
+                <span>The Monster The Monster The Monster</span>
+                <span>The Monster The Monster The Monster</span>
+                <span>The Monster The Monster The Monster</span>
+                <span>The Monster The Monster The Monster</span>
+            </div>
+        </section>
+    );
+}
+
+export default PlayMusic;
