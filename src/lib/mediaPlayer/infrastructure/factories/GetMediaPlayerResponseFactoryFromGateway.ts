6163c7fb630592bc18392dba3272ee0b4874cc94
Noubissie Pavel <noubissiepavel@gmail.com>
2024-09-13 01:03:56 +0100

    [fix] - right adapter corrected

--- src/lib/mediaPlayer/infrastructure/factories/GetMediaPlayerResponseFactoryFromGateway.ts
+++ src/lib/mediaPlayer/infrastructure/factories/GetMediaPlayerResponseFactoryFromGateway.ts
@@ -1,6 +1,20 @@
-import {GetAllMediaPlayerResponseGateway} from "../responses/GetAllMediaPlayerResponseGateway";
 import {GetAllMediaPlayerResponse} from "../../usecases/GetMediaPlayer/GetAllMediaPlayerResponse";
+import {GetAllMediaPlayerResponseGateway} from "../responses/GetAllMediaPlayerResponseGateway";
 
-export const GetMediaPlayerResponseFactoryFromGateway = (data: GetAllMediaPlayerResponseGateway[]): GetAllMediaPlayerResponse[] => {
-    return data;
-}
+export const GetMediaPlayerResponseFactoryFromGateway = (
+    data: GetAllMediaPlayerResponseGateway[]
+): GetAllMediaPlayerResponse[] => {
+    return data.map((item) => ({
+        id: item.id,
+        title: item.title,
+        artist: item.artist,
+        album: item.album,
+        genre: item.genre,
+        duration: item.duration,
+        trackCount: item.trackCount,
+        year: item.year,
+        lyrics: item.lyrics,
+        coverPath: item.coverPath,
+        mp3Data: item.mp3Data
+    }));
+};
--- src/lib/shared/gateway/HttpProvider.ts
+++ src/lib/shared/gateway/HttpProvider.ts
@@ -1,13 +1,13 @@
 export abstract class HttpProvider {
     // private baseUrl: string = env.MEDIA_PLAYER_ENDPOINT;
-    private baseUrl: string = 'http://localhost:8080/';
+    private baseUrl: string = 'json/';
 
     public async get(url: string, query?: object): Promise<Response> {
         let queryUrl: string = '';
         if (query) {
             queryUrl = '?' + new URLSearchParams(Object.entries(query)).toString();
         }
-       return  await fetch(this.getUrl(url + queryUrl), {
+        return await fetch(this.getUrl(url + queryUrl), {
             headers: {
                 'Content-Type': 'application/json'
             }
