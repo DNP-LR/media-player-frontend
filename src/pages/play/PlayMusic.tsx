e3ea59f0f3132372d4a95b62c6360150f5b5e1d0
Noubissie Pavel <noubissiepavel@gmail.com>
2024-09-13 11:20:01 +0100

    [feat] - framer motion

--- src/pages/play/PlayMusic.tsx
+++ src/pages/play/PlayMusic.tsx
@@ -1,14 +1,38 @@
 import {usePage} from "../usePage";
+import React from "react";
+import {twMerge} from "tailwind-merge";
+import {useOutletContext} from "react-router-dom";
+import {motion} from "framer-motion"
+
+interface PlayHomeContext {
+    isPlaying: boolean;
+}
 
 const PlayMusic = () => {
     const {handleNavigateToPlayWithLyrics, currenMusic} = usePage();
-
+    const {isPlaying} = useOutletContext<PlayHomeContext>();
     return (
         <section className="">
             <div className="flex items-center justify-center  shadow-amber-500 ">
-                <img className="h-full max-h-[60%] animate-spin  max-w-[60%] w-full rounded-full"
-                     src={currenMusic?.coverPath ?? 'Default value'}
-                     alt="Union"/>
+                <motion.div
+                    className="items-center flex justify-center h-full max-h-[60%]  max-w-[60%] w-full rounded-full"
+                    initial={{opacity: 0, scale: 0.9}}
+                    animate={{opacity: 1, scale: 1}}
+                    transition={{
+                        duration: 0.3,
+                        ease: [0, 0.71, 0.2, 1.01],
+                        scale: {
+                            type: "spring",
+                            damping: 5,
+                            stiffness: 100,
+                            restDelta: 0.001
+                        }
+                    }}>
+                    <img className={twMerge("",
+                        isPlaying ? "animate-spin" : "")}
+                         src={currenMusic?.coverPath ?? 'Default value'}
+                         alt="Union"/>
+                </motion.div>
             </div>
             <div
                 className="flex flex-col cursor-pointer items-center justify-center h-full max-h-[80%] text-sm text-center"
--- src/pages/playlist/PlayListMobile.tsx
+++ src/pages/playlist/PlayListMobile.tsx
@@ -5,65 +5,73 @@ import IconPause from "../../ui/shared/icons/IconPause";
 import IconPlay from "../../ui/shared/icons/IconPlay";
 import {twMerge} from "tailwind-merge";
 import {MediaPlayer} from "../../lib/mediaPlayer/model/MediaPlayer";
-import {usePage} from "../usePage";
 
 
 interface PlayListMobileProps {
     handleNavigateToPlay: () => void;
-    mediaPlayerList: MediaPlayer[]
+    mediaPlayerList: MediaPlayer[];
+    setCurrenMusic: (currenMusic: MediaPlayer) => void;
+    currenMusic: MediaPlayer;
+    handlePlayPause: () => void;
+    isPlaying: boolean;
 }
 
-const MusicPlayer = ({handleNavigateToPlay, mediaPlayerList}: PlayListMobileProps) => {
-    const {currenMusic, setCurrenMusic} = usePage();
-    return (
-        <div
-            className="h-full lg:rounded-2xl lg:shadow-2xl bg-primaryColor-primaryOne w-full lg:max-w-[25%] lg:max-h-[90%] flex flex-col justify-between">
-            <div className="h-full max-h-[25%] relative">
-                <div className="flex justify-center items-center">
-                    <div>
+const MusicPlayer =
+    ({
+         handleNavigateToPlay,
+         isPlaying,
+         handlePlayPause,
+         currenMusic,
+         setCurrenMusic,
+         mediaPlayerList
+     }: PlayListMobileProps) => {
+        return (
+            <div
+                className="h-full lg:rounded-2xl lg:shadow-2xl bg-primaryColor-primaryOne w-full lg:max-w-[25%] lg:max-h-[90%] flex flex-col justify-between">
+                <div className="h-full max-h-[25%] relative">
+                    <div className="flex justify-center items-center">
                         <img
                             src={currenMusic.coverPath ?? 'Default value'}
                             alt="Album Cover"
-                            className="object-cover w-full h-full"
+                            className="object-cover items-center justify-center flex max-h-[60%]  max-w-[60%] rounded-full w-full h-full"
                         />
                     </div>
                 </div>
-            </div>
-            <div className="flex p-2 pl-8 pr-8 items-center justify-between">
-                <button className="p-4 rounded-full bg-primaryColor-primaryTwo shadow-2xl hover:bg-gray-900 ">
-                    <IconDownload/>
-                </button>
-                <button className="p-4 rounded-full bg-primaryColor-primaryTwo shadow-2xl hover:bg-gray-900 ">
-                    <IconLike className="bg-secondaryColor-secondaryTwo"/>
-                </button>
-            </div>
-            <div
-                className="bg-gray-800 rounded-l-2xl h-full max-h-[65%] bottom-0 overflow-y-scroll rounded-r-2xl  p-4 flex flex-col space-y-4">
-                {mediaPlayerList.map((song, index) => {
-                    return (
-                        <div
-                            key={'song' + index}
-                            onClick={() => setCurrenMusic(song)}
-                            className={twMerge(
-                                currenMusic === song ? "bg-primaryColor-primaryTwo bg-opacity-2" : "",
-                                "flex hover:bg-primaryColor-primaryTwo hover:bg-opacity-2 justify-between items-center p-3 rounded-lg cursor-pointer"
-                            )}>
-                            <div className="flex items-center">
-                                <div className="flex flex-col justify-start">
-                                    <p className="text-lg font-primary">{song.title ?? 'Default value'}</p>
-                                    <p className="text-sm font-primary text-gray-400">{song.artist ?? 'Default value'}</p>
+                <div className="flex p-2 pl-8 pr-8 items-center justify-between">
+                    <button className="p-4 rounded-full bg-primaryColor-primaryTwo shadow-2xl hover:bg-gray-900 ">
+                        <IconDownload/>
+                    </button>
+                    <button className="p-4 rounded-full bg-primaryColor-primaryTwo shadow-2xl hover:bg-gray-900 ">
+                        <IconLike className="bg-secondaryColor-secondaryTwo"/>
+                    </button>
+                </div>
+                <div
+                    className="bg-gray-800 rounded-l-2xl h-full max-h-[65%] bottom-0 overflow-y-scroll rounded-r-2xl  p-4 flex flex-col space-y-4">
+                    {mediaPlayerList.map((song, index) => {
+                        return (
+                            <div
+                                key={'song' + index}
+                                onClick={() => setCurrenMusic(song)}
+                                className={twMerge(
+                                    currenMusic === song ? "bg-primaryColor-primaryTwo bg-opacity-2" : "",
+                                    "flex hover:bg-primaryColor-primaryTwo hover:bg-opacity-2 justify-between items-center p-3 rounded-lg cursor-pointer"
+                                )}>
+                                <div className="flex items-center">
+                                    <div className="flex flex-col justify-start">
+                                        <p className="text-lg font-primary">{song.title ?? 'Default value'}</p>
+                                        <p className="text-sm font-primary text-gray-400">{song.artist ?? 'Default value'}</p>
+                                    </div>
+                                </div>
+                                <div onClick={handleNavigateToPlay}
+                                     className="flex justify-end p-2 rounded-full bg-orange-500">
+                                    {currenMusic === song ? <IconPause/> : <IconPlay/>}
                                 </div>
                             </div>
-                            <div onClick={handleNavigateToPlay}
-                                 className="flex justify-end p-2 rounded-full bg-orange-500">
-                                {currenMusic === song ? <IconPause/> : <IconPlay/>}
-                            </div>
-                        </div>
-                    )
-                })}
+                        )
+                    })}
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    };
 
 export default MusicPlayer;
