f8c3544ac4d0e696c578fb0f4fa1d52f9204233a
Noubissie Pavel <noubissiepavel@gmail.com>
2024-09-13 11:20:14 +0100

    [feat] - framer motion

--- src/pages/playlist/PlayList.tsx
+++ src/pages/playlist/PlayList.tsx
@@ -1,14 +1,20 @@
 import {usePage} from "../usePage";
 import WrapperPage from "../../ui/layout/WrapperPage";
 import MusicPlayer from "./PlayListMobile";
+import {usePlay} from "../play/usePlay";
 
 const PlayList = () => {
-    const {handleNavigateToPlay, mediaPlayerViewModel} = usePage();
+    const {handleNavigateToPlay, mediaPlayerViewModel, currenMusic, setCurrenMusic} = usePage();
+    const {isPlaying, handlePlayPause,} = usePlay();
     return (
         <WrapperPage>
             <MusicPlayer
                 handleNavigateToPlay={handleNavigateToPlay}
                 mediaPlayerList={mediaPlayerViewModel.mediaPlayerList}
+                currenMusic={currenMusic}
+                setCurrenMusic={setCurrenMusic}
+                isPlaying={isPlaying}
+                handlePlayPause={handlePlayPause}
             />
         </WrapperPage>
     )
